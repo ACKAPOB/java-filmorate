d3b7b0fd7bbf8ee185a49ad0094f033af4fae4d0
ACKAPOB <ACKAPOB@yandex.ru>
2022-08-15 16:13:00 +0300

    15.08.22 insert на merge, базу с файла на память

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO mpa_rates (mpa_id, name, description)
+merge into mpa_rates (mpa_id, name, description)
 VALUES (1, 'G', 'у фильма нет возрастных ограничений'),
        (2, 'PG', 'детям рекомендуется смотреть фильм с родителями'),
        (3, 'PG-13', 'детям до 13 лет просмотр не желателен'),
        (4, 'R', 'лицам до 17 лет просматривать фильм можно только в присутствии взрослого'),
        (5, 'NC-17', 'лицам до 18 лет просмотр запрещён');
 
-INSERT INTO genres (genre_id, name)
+merge into genres (genre_id, name)
 VALUES (1, 'Комедия'),
        (2, 'Драма'),
        (3, 'Мультфильм'),
